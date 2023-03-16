095fc33b6371b945bb4e9f9c8a9fa301b80eeffa
abdelilah-zarrar <abdelilahzarrar@gmail.com>
2023-03-16 14:35:28 -0700

    task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,13 @@
 #include <stdio.h>
-
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
-    puts("Programming is like building a multilingual puzzle");
-    return 0;
+	char *message = "\"Programming is like building a multilingual puzzle";
+
+	puts(message);
+	return (0);
 }
