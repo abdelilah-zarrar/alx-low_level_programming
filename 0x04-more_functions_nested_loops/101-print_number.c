01f69d14303ba49d7e2cfaaec84b41c76831f668
abdelilah-zarrar <abdelilahzarrar@gmail.com>
2023-03-23 19:55:55 -0700

    task tazbal 12

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -1,35 +1,22 @@
 #include "main.h"
 
 /**
- * print_number - print an integer, without using long, arrays, or pointers
+ * print_number - prints numbers
  * @n: number to be printed
+ * Return:void
  */
 
 void print_number(int n)
 {
-	unsigned int tens, digit, positive = n;
-	double t_beg = 1;
+	unsigned int x;
 
-	if (n == 0)
-		_putchar('0');
-	else
+	x = n;
+	if (n < 0)
 	{
-		if (n < 0)
-		{
-			positive = n * -1;
-			_putchar('-');
-		}
-
-		while (t_beg <= positive)
-			t_beg *= 10;
-		tens = t_beg / 10;
-
-		while (tens >= 1)
-		{
-			digit = positive / tens;
-			_putchar(digit + '0');
-			positive = (positive - (tens * digit));
-			tens /= 10;
-		}
+		_putchar(45);
+		x = -n;
 	}
+	if (x / 10)
+		print_number(x / 10);
+	_putchar((x % 10) + '0');
 }
