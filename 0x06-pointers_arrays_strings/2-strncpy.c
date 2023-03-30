1b502980fc0053bbeb75c376a3e831fb3ac4b3e0
abdelilah-zarrar <abdelilahzarrar@gmail.com>
2023-03-30 16:43:35 -0700

    task 2

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -1,32 +1,18 @@
 #include "main.h"
-
 /**
- * _strncpy - function that concatenates two strings
- * @src: source string
- * @dest: destination string
- * @n: destination string
- * Return: pointer to the resulting string dest
+ * _strncpy - concatenate strings, defining the size of second string.
+ * @dest: string with concatenation
+ * @src: string to be concatenated
+ * @n: size of second string
+ * Return: Always 0.
  */
 char *_strncpy(char *dest, char *src, int n)
 {
 	int i;
-	int j;
 
-	for (i = 0; dest[i] != '\0'; i++)
-	{
-		if (dest[i + 1] == '\0')
-		{
-			for (j = 0; src[j] != '\0' && j < n; j++)
-			{
-				dest[j] = src[j];
-				i++;
-			}
-			while (i < n)
-			{
-				dest[i] = '\0';
-				i++;
-			}
-		}
-	}
+	for (i = 0; i < n && src[i] != '\0'; i++)
+		*(dest + i) = *(src + i);
+	for ( ; i < n; i++)
+		*(dest + i) = '\0';
 	return (dest);
 }
