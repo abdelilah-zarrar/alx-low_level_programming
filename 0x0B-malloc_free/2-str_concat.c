32a2302f0bf8328a82c3f6637fa0dd510d82e9fb
abdelilah-zarrar <abdelilahzarrar@gmail.com>
2023-04-11 05:50:50 -0700

    jh

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -11,34 +11,34 @@
  */
 char *str_concat(char *s1, char *s2)
 {
-    char *concat_str;
-    int s1_len = 0, s2_len = 0, i, j;
+char *concat_str;
+int s1_len = 0, s2_len = 0, i, j;
 
-    /* Handle NULL strings as empty strings */
-    if (s1 == NULL)
-        s1 = "";
-    if (s2 == NULL)
-        s2 = "";
+/* Handle NULL strings as empty strings */
+if (s1 == NULL)
+s1 = "";
+if (s2 == NULL)
+s2 = "";
 
-    /* Find the lengths of the strings */
-    while (*(s1 + s1_len) != '\0')
-        s1_len++;
-    while (*(s2 + s2_len) != '\0')
-        s2_len++;
+/* Find the lengths of the strings */
+while (*(s1 + s1_len) != '\0')
+s1_len++;
+while (*(s2 + s2_len) != '\0')
+s2_len++;
 
-    /* Allocate memory for the concatenated string */
-    concat_str = malloc(sizeof(char) * (s1_len + s2_len + 1));
-    if (concat_str == NULL)
-        return (NULL);
+/* Allocate memory for the concatenated string */
+concat_str = malloc(sizeof(char) * (s1_len + s2_len + 1));
+if (concat_str == NULL)
+return (NULL);
 
-    /* Copy the contents of s1 and s2 into the concatenated string */
-    for (i = 0; i < s1_len; i++)
-        *(concat_str + i) = *(s1 + i);
-    for (j = 0; j < s2_len; j++)
-        *(concat_str + i + j) = *(s2 + j);
+/* Copy the contents of s1 and s2 into the concatenated string */
+for (i = 0; i < s1_len; i++)
+*(concat_str + i) = *(s1 + i);
+for (j = 0; j < s2_len; j++)
+*(concat_str + i + j) = *(s2 + j);
 
-    /* Add null terminator */
-    *(concat_str + i + j) = '\0';
+/* Add null terminator */
+*(concat_str + i + j) = '\0';
 
-    return (concat_str);
+return (concat_str);
 }
