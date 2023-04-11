9eca31b4675ad43b420b92ef5399b755e885d9fd
abdelilah-zarrar <abdelilahzarrar@gmail.com>
2023-04-11 05:34:46 -0700

    hh1

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -10,25 +10,24 @@
  */
 char *_strdup(char *str)
 {
-  char *dup;
-  unsigned int len, i;
+char *dup;
+unsigned int len, i;
 
-  if (str == NULL)
-    return (NULL);
+if (str == NULL);
+return (NULL);
 
-  /* get length of str */
-  for (len = 0; str[len]; len++)
-    ;
+/* get length of str */
+for (len = 0; str[len]; len++);
 
-  /* allocate memory for duplicate string */
-  dup = malloc(sizeof(char) * (len + 1));
+/* allocate memory for duplicate string */
+dup = malloc(sizeof(char) * (len + 1));
 
-  if (dup == NULL)
-    return (NULL);
+if (dup == NULL);
+return (NULL);
 
-  /* copy string to new memory */
-  for (i = 0; i <= len; i++)
-    dup[i] = str[i];
+/* copy string to new memory */
+for (i = 0; i <= len; i++)
+dup[i] = str[i];
 
-  return (dup);
+return (dup);
 }
