9230f842ff82ba6383c1a626ba3ef81cbd351e02
abdelilah-zarrar <abdelilahzarrar@gmail.com>
2023-04-11 19:46:11 -0700

    betty betch

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -1,296 +1,73 @@
 #include <stdlib.h>
-
-                
-
 #include "main.h"
-
-                
-
-
-                
-
+#include <string.h>
 /**
-
-                
-
- *  * count_word - helper function to count the number of words in a string
-
-                
-
- *   * @s: string to evaluate
-
-                
-
- *    *
-
-                
-
- *     * Return: number of words
-
-                
-
- *      */
-
-                
-
-int count_word(char *s)
-
-                
-
+ * get_nwords - get the number of space separated words in `s'
+ * @s: string to search
+ * Return: number of words in `s'
+ */
+int get_nwords(char *s)
 {
+	int i = 0, nnwords = 0;
 
-                
-
-                int flag, c, w;
-
-                
-
-
-                
-
-                        flag = 0;
-
-                
-
-                                w = 0;
-
-                
-
-
-                
-
-                                        for (c = 0; s[c] != '\0'; c++)
-
-                
-
-                                                        {
-
-                
-
-                                                                                if (s[c] == ' ')
-
-                
-
-                                                                                                                flag = 0;
-
-                
-
-                                                                                                else if (flag == 0)
-
-                
-
-                                                                                                                        {
-
-                
-
-                                                                                                                                                        flag = 1;
-
-                
-
-                                                                                                                                                                                w++;
-
-                
-
-                                                                                                                                                                                                }
-
-                
-
-                                                                                                        }
-
-                
-
-
-                
-
-                                                return (w);
-
-                
-
+	while (s[i])
+	{
+		while (s[i] && s[i] == ' ')
+			++i;
+		if (s[i] == '\0')
+			break;
+		while (s[i] && s[i] != ' ')
+			++i;
+		++nnwords;
+	}
+	return (nnwords);
 }
 
-                
-
 /**
-
-                
-
- * **strtow - splits a string into words
-
-                
-
- * @str: string to split
-
-                
-
+ * strtow - split `str' into array of words using spaces to delimit words
+ * @str: string of space-separated words
  *
-
-                
-
- * Return: pointer to an array of strings (Success)
-
-                
-
- * or NULL (Error)
-
-                
-
+ * Return: pointer to array of strings, or NULL on failure
  */
 
-                
-
 char **strtow(char *str)
-
-                
-
 {
-
-                
-
-        char **matrix, *tmp;
-
-                
-
-        int i, k = 0, len = 0, words, c = 0, start, end;
-
-                
-
-
-                
-
-        while (*(str + len))
-
-                
-
-                len++;
-
-                
-
-        words = count_word(str);
-
-                
-
-        if (words == 0)
-
-                
-
-                return (NULL);
-
-                
-
-
-                
-
-        matrix = (char **) malloc(sizeof(char *) * (words + 1));
-
-                
-
-        if (matrix == NULL)
-
-                
-
-                return (NULL);
-
-                
-
-
-                
-
-        for (i = 0; i <= len; i++)
-
-                
-
-        {
-
-                
-
-                if (str[i] == ' ' || str[i] == '\0')
-
-                
-
-                {
-
-                
-
-                        if (c)
-
-                
-
-                        {
-
-                
-
-                                end = i;
-
-                
-
-                                tmp = (char *) malloc(sizeof(char) * (c + 1));
-
-                
-
-                                if (tmp == NULL)
-
-                
-
-                                        return (NULL);
-
-                
-
-                                while (start < end)
-
-                
-
-                                        *tmp++ = str[start++];
-
-                
-
-                                *tmp = '\0';
-
-                
-
-                                matrix[k] = tmp - c;
-
-                
-
-                                k++;
-
-                
-
-                                c = 0;
-
-                
-
-                        }
-
-                
-
-                }
-
-                
-
-                else if (c++ == 0)
-
-                
-
-                        start = i;
-
-                
-
-        }
-
-                
-
-
-                
-
-        matrix[k] = NULL;
-
-                
-
-
-                
-
-        return (matrix);
-
-                
-
+	int i, n, m, nwords, ennd, beggin;
+	char **pt;
+
+	if (str == NULL || *str == '\0')
+		return (NULL);
+	nwords = get_nwords(str);
+	if (nwords == 0)
+		return (NULL);
+	++nwords;
+	pt = (char **) malloc(nwords * sizeof(char *));
+	if (pt == NULL)
+		return (NULL);
+	i = n = 0;
+	while (str[i])
+	{
+		while (str[i] && str[i] == ' ')
+			++i;
+		if (str[i] == '\0')
+			break;
+		beggin = i;
+		while (str[i] && str[i] != ' ')
+			++i;
+		ennd = i;
+		pt[n] = (char *) malloc((ennd - beggin + 1) * sizeof(char));
+		if (pt[n] == NULL)
+		{
+			free(pt[n]);
+			while (n)
+				free(pt[--n]);
+			free(pt);
+			return (NULL);
+		}
+		for (m = 0; m < (ennd - beggin); ++m)
+			pt[n][m] = str[beggin + m];
+		pt[n++][m] = '\0';
+	}
+	pt[n] = NULL;
+	return (pt);
 }
