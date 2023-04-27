faf66e8188f067bc009a856a2217a146138a6325
abdelilah-zarrar <abdelilahzarrar@gmail.com>
2023-04-27 10:13:47 -0700

    modified task 0

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -9,17 +9,18 @@
  */
 size_t print_list(const list_t *h)
 {
-size_t count = 0;
+size_t count;
 
+count = 0;
 while (h != NULL)
 {
 if (h->str == NULL)
-printf("[0] (nil)\n");
+printf("[%d] %s\n", 0, "(nil)");
 else
 printf("[%d] %s\n", h->len, h->str);
 
-count++;
 h = h->next;
+count++;
 }
 
 return (count);
