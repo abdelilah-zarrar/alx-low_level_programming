7e46d40d0065f3614c1270d0f3689071d9723171
abdelilah-zarrar <abdelilahzarrar@gmail.com>
2023-04-27 08:51:17 -0700

    task 2 and edit lists.h

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -1,6 +1,9 @@
 #ifndef LISTS_H
 #define LISTS_H
 
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 #include <stddef.h>
 
 /**
@@ -12,6 +15,15 @@
  * Description: singly linked list node structure
  * for Holberton project
  */
+
+/* struct definition for listint_t */
+typedef struct listint_s
+{
+    int n;
+    struct listint_s *next;
+} listint_t;
+
+/* struct definition for list_t */
 typedef struct list_s
 {
     char *str;
